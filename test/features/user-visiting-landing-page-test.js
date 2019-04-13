c73d5dcd1e9db796b6dbc5d19ad52c5d1a5c88ea
SalmonKing72 <madichjm@gmail.com>
2019-04-13 14:53:12 -0500

    create video feature red

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -2,9 +2,17 @@ const {assert} = require('chai');
 
 describe('User visits root', () => {
     describe('without existing videos', () => {
-      it('starts blank', () => {
-        browser.url('/');
-        assert.equal(browser.getText('#videos-container'), '');
-      });
+        it('starts blank', () => {
+            browser.url('/');
+            assert.equal(browser.getText('#videos-container'), '');
+        });
     });
+
+    describe('to create a new item', () => {
+        it('provides a form to create a new item', () => {
+            browser.url('/');
+            browser.click('a[href="/videos/create.html"]');
+            assert.include(browser.getText('a[id="create-video-link"]'), 'Save a video');
+        });
+    })
 });
