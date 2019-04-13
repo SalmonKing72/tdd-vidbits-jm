49df24ffe6bf8dccf811dc51abd275b164279336
SalmonKing72 <madichjm@gmail.com>
2019-04-13 14:25:57 -0500

    initial request root feature with no videos red

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,10 @@
 const {assert} = require('chai');
+
+describe('User visits root', () => {
+    describe('without existing videos', () => {
+      it('starts blank', () => {
+        browser.url('/');
+        assert.equal(browser.getText('#videos-container'), '');
+      });
+    });
+});
