dc9e899d1b552880169f0b50aab0c43d9741a5a8
SalmonKing72 <madichjm@gmail.com>
2019-04-15 20:37:48 -0400

    test create and persist videos to database red

--- test/routes/create-test.js
+++ test/routes/create-test.js
@@ -4,11 +4,16 @@ const request = require('supertest');
 const app = require('../../app');
 const Video = require('../../models/video');
 
-const {parseTextFromHTML, buildItemObject} = require('../test-utils');
+const {buildItemObject} = require('../test-utils');
+const {connectDatabaseAndDropData, disconnectDatabase} = require('../database-utils');
 
 describe('Server path: /videos', () => {
+    beforeEach(connectDatabaseAndDropData);
+
+    afterEach(disconnectDatabase);
+
     describe('POST', () => {
-        it('creates a video and persists it', async () => {
+        it('responds successfully', async () => {
             const videoToCreate = buildItemObject();
 
             const response = await request(app)
@@ -17,6 +22,18 @@ describe('Server path: /videos', () => {
                 .send(videoToCreate);
 
             assert.strictEqual(response.status, 201);
-        })
+        });
+
+        it('creates a video and persists it', async () => {
+            const videoToCreate = buildItemObject();
+
+            const response = await request(app)
+                .post('/videos')
+                .type('form')
+                .send(videoToCreate);
+
+            let createdVideo = await Video.findOne(videoToCreate);
+            assert.isNotNull(createdVideo, 'the video was not created in the database');
+        });
     });
 });
