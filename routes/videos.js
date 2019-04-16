3313305763b8780a9447e15ef46daad43f626e3e
SalmonKing72 <madichjm@gmail.com>
2019-04-15 21:02:32 -0400

    Video model and server tests green

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
+const Video = require('../models/video');
 
 router.post('/videos', async (req, res, err) => {
+    const videoTitle = req.body.title;
+    const videoDescription = req.body.description;
+
+    await Video.create({
+        title: videoTitle,
+        description: videoDescription
+    });
+    
     res.status(201).send();
 });
 
