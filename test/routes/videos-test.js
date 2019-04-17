3b756b8dabd50a041ad44c40e51e4bef7de4d73f
SalmonKing72 <madichjm@gmail.com>
2019-04-16 22:03:46 -0400

    posting item without title test red

--- test/routes/videos-test.js
+++ test/routes/videos-test.js
@@ -2,9 +2,10 @@ const {assert} = require('chai');
 const request = require('supertest');
 const {jsdom} = require('jsdom');
 
+const Video = require('../../models/video');
 const app = require('../../app');
 
-const {parseTextFromHTML, seedItemToDatabase} = require('../test-utils');
+const {parseTextFromHTML, buildItemObject, seedItemToDatabase} = require('../test-utils');
 const {connectDatabase, disconnectDatabase} = require('../database-utilities');
 
 describe('Server path: /videos', () => {
@@ -12,6 +13,13 @@ describe('Server path: /videos', () => {
     afterEach(disconnectDatabase);
 
     describe('GET', () => {
+        it('redirects to the right location', async () => {
+            const response = await request(app)
+                .get('/');
+
+            assert.strictEqual(response.status, 302);
+        });
+
         it('renders an item with a title and image', async () => {
             const video = await seedItemToDatabase();
       
@@ -21,5 +29,48 @@ describe('Server path: /videos', () => {
             assert.include(parseTextFromHTML(response.text, '#videos-container'), video.title);
             assert.include(parseTextFromHTML(response.text, '#videos-container'), video.description);
           });
+    });
+
+    describe('POST', () => {
+        it('responds successfully', async () => {
+            const videoToCreate = buildItemObject();
+
+            const response = await request(app)
+                .post('/videos')
+                .type('form')
+                .send(videoToCreate);
+
+            assert.strictEqual(response.status, 201);
+            assert.include(response.text, videoToCreate.title);
+            assert.include(response.text, videoToCreate.description);
+        });
+
+        it('does not persist a video without a title', async () => {
+            const invalidItemToCreate = {
+                description: 'test',
+            };
+        
+            const response = await request(app)
+                .post('/videos')
+                .type('form')
+                .send(invalidItemToCreate);
+    
+            const videos = await Video.find({});
+    
+            assert.equal(videos.length, 0);
+            // assert.strictEqual(response.status, 400);
+        })
+
+        it('creates a video and persists it', async () => {
+            const videoToCreate = buildItemObject();
+
+            const response = await request(app)
+                .post('/videos')
+                .type('form')
+                .send(videoToCreate);
+
+            let createdVideo = await Video.findOne(videoToCreate);
+            assert.isNotNull(createdVideo, 'the video was not created in the database');
+        });
     });
 });
