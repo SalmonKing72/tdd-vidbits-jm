58a772905d32e5a896802d7d9814f3616e9f265b
SalmonKing72 <madichjm@gmail.com>
2019-04-17 23:01:11 -0400

    refactor feature video setup into own function

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -5,6 +5,15 @@ const generateRandomUrl = (domain) => {
     return `http://${domain}/${Math.random()}`;
 };
 
+const setupVideoData = (video) => {
+    browser.url('/videos/create');
+    browser.setValue('#title-input', video.title);
+    browser.setValue('#description-input', video.description);
+    browser.setValue('#videoUrl-input', video.videoUrl);
+    browser.click('#submit-button');
+    browser.url('/');
+}
+
 describe('User visits root', () => {
     describe('without existing videos', () => {
         it('starts blank', () => {
@@ -16,25 +25,18 @@ describe('User visits root', () => {
     describe('with an existing video', () => {
         it('displays the video', () => {
             let video = buildItemObject();
-            browser.url('/videos/create');
-            browser.setValue('#title-input', video.title);
-            browser.setValue('#description-input', video.description);
-            browser.setValue('#videoUrl-input', video.videoUrl);
-            browser.click('#submit-button');
-            browser.url('/');
 
+            setupVideoData(video);
+    
             assert.include(browser.getText('#videos-container'), video.title);
             assert.include(browser.getText('#videos-container'), video.description);
         });
 
         it('can navigate to a single video', () => {
             let video = buildItemObject();
-            browser.url('/videos/create');
-            browser.setValue('#title-input', video.title);
-            browser.setValue('#description-input', video.description);
-            browser.setValue('#videoUrl-input', video.videoUrl);
-            browser.click('#submit-button');
-            browser.url('/');
+
+            setupVideoData(video);
+
             browser.click('.video-title a');
             assert.include(browser.getText('.video-title'), video.title);
             assert.include(browser.getText('.video-description'), video.description);
