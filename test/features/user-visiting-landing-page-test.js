c42a495c6b74f34119d9e964d47ed508de51b091
SalmonKing72 <madichjm@gmail.com>
2019-04-17 22:58:33 -0400

    title links to single video view test green

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,6 +1,10 @@
 const {assert} = require('chai');
 const {buildItemObject} = require('../test-utils');
 
+const generateRandomUrl = (domain) => {
+    return `http://${domain}/${Math.random()}`;
+};
+
 describe('User visits root', () => {
     describe('without existing videos', () => {
         it('starts blank', () => {
@@ -21,6 +25,19 @@ describe('User visits root', () => {
 
             assert.include(browser.getText('#videos-container'), video.title);
             assert.include(browser.getText('#videos-container'), video.description);
+        });
+
+        it('can navigate to a single video', () => {
+            let video = buildItemObject();
+            browser.url('/videos/create');
+            browser.setValue('#title-input', video.title);
+            browser.setValue('#description-input', video.description);
+            browser.setValue('#videoUrl-input', video.videoUrl);
+            browser.click('#submit-button');
+            browser.url('/');
+            browser.click('.video-title a');
+            assert.include(browser.getText('.video-title'), video.title);
+            assert.include(browser.getText('.video-description'), video.description);
         })
     })
 
@@ -30,5 +47,7 @@ describe('User visits root', () => {
             browser.click('a[id="create-video-link"]');
             assert.include(browser.getText('button[id="submit-button"]'), 'Save Video');
         });
-    })
+    });
+
+    
 });
